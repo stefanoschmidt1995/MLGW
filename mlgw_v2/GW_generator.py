Gradients and nu convention fixed. Added lal

mlgw_v2/GW_generator.py
@@ -248,10 +248,10 @@ GW_generator
 			h_plus, h_cross (1,D)/(N,D)		desidered polarizations
 		"""
 		theta = np.column_stack((m1, m2, spin1_x, spin1_y, spin1_z, spin2_x, spin2_y, spin2_z, D_L, i, phi_0, long_asc_nodes, eccentricity, mean_per_ano)) #(N,D)
-		return self.get_WF(theta, t_grid= t_grid, modes = None)
+		return self.get_WF(theta, t_grid= t_grid, modes = (2,2))
 
 
-	def get_WF(self, theta, t_grid, modes = None):
+	def get_WF(self, theta, t_grid, modes = (2,2) ):
 		"""
 	get_WF
 	======
@@ -333,7 +333,7 @@ GW_generator
 		"""
 	__get_WF
 	========
-		Generates the waves in time domain, building it as a sum of modes weighted by spherical harminics. Called by get_WF.
+		Generates the waves in time domain, building it as a sum of modes weighted by spherical harmonics. Called by get_WF.
 		Accepts only input features as [q,s1,s2] or [m1, m2, spin1_z , spin2_z, D_L, inclination, phi_0].
 		Input:
 			theta (N,D)		source parameters to make prediction at (D=7)
@@ -348,9 +348,7 @@ GW_generator
 			#computing amplitude prefactor
 		prefactor = 4.7864188273360336e-20 # G/c^2*(M_sun/Mpc)
 		m_tot_us = theta[:,0] + theta[:,1]	#total mass in solar masses for the user  (N,)
-		nu_us = theta[:,0]/ theta[:,1] #q
-		nu_us = np.divide(nu_us, np.square(1+nu_us)) #nu = q/(1+q)**2
-		amp_prefactor = prefactor*nu_us*m_tot_us/theta[:,4] # G/c^2 (nu * M / d_L) apparently nu is there??? Whyyyy?
+		amp_prefactor = prefactor*m_tot_us/theta[:,4] # G/c^2 (M / d_L) 
 
 		h_plus = np.zeros((theta.shape[0],t_grid.shape[0]))
 		h_cross = np.zeros((theta.shape[0],t_grid.shape[0]))
@@ -369,7 +367,7 @@ GW_generator
 
 		return h_plus, h_cross
 
-	def get_modes(self, theta, t_grid, modes = None, out_type = "ampph"):
+	def get_modes(self, theta, t_grid, modes = (2,2), out_type = "ampph"):
 		"""
 	get_modes
 	=========
@@ -445,7 +443,7 @@ GW_generator
 		d_lm = self.__get_Wigner_d_function((l,m),iota) #(N,)
 		d_lmm = self.__get_Wigner_d_function((l,-m),iota) #(N,)
 		const = np.sqrt( (2.*l+1.)/(4.*np.pi) )
-		parity = np.power(-1,l)
+		parity = np.power(-1,l) #are you sure of that? apparently yes...
 
 		h_lm_real = np.multiply(np.multiply(amp.T,np.cos(ph.T+m*phi_0)), const*(d_lm + parity * d_lmm) ).T #(N,D)
 		h_lm_imag = np.multiply(np.multiply(amp.T,np.sin(ph.T+m*phi_0)), const*(d_lm - parity * d_lmm) ).T #(N,D)
@@ -928,8 +926,8 @@ mode_generator
 		"""
 	get_raw_grads
 	=============
-		Computes the gradients (at points theta) of the amplitude and phase w.r.t. (q,s1,s2).
-		Gradients are functions dependent on time and are evaluated on the internal reduced grid (GW_generator.get_time_grid()).
+		Computes the gradients of the amplitude and phase w.r.t. (q,s1,s2).
+		Gradients are functions dependent on time and are evaluated on the internal reduced grid (mode_generator.get_time_grid()).
 		Input:
 			theta (N,3)		Values of orbital parameters to compute the gradient at
 		Output:
@@ -960,10 +958,10 @@ mode_generator
 
 		return grad_amp, grad_ph
 
-	def get_grads(self, theta, t_grid):
+	def get_mode_grads(self, theta, t_grid, out_type ="realimag"):
 		"""
-	__get_grads_theta
-	=================
+	get_mode_grads
+	==============
 		Returns the gradient of the mode
 			hlm = A exp(1j*phi) = A cos(phi) + i* A sin(phi)
 		with respect to theta = (M, q, s1, s2).
@@ -972,6 +970,7 @@ mode_generator
 		Input:
 			theta (N,4)		orbital parameters with format (m1, m2, s1, s2)
 			t_grid (D,)		time grid to evaluate the gradients at
+			out_type		whether to compute gradients of the real and imaginary part ('realimag') or of amplitude and phase ('ampph')
 		Output:
 			grad_Re(h) (N,D,4)		Gradients of the real part of the waveform
 			grad_Im(h) (N,D,4)		Gradients of the imaginary part of the waveform
@@ -991,22 +990,21 @@ mode_generator
 
 		#dealing with gradients w.r.t. (q,s1,s2)
 		grad_q_amp, grad_q_ph = self.get_raw_grads(theta_std) #(N,D_std,3)
-		grad_q_amp = 1e-21*grad_q_amp
-		m_tot_std = 20.
+		grad_q_amp = grad_q_amp
 			#interpolating gradients on the user grid
 		for i in range(theta_std.shape[0]):
 			for j in range(1,4):
 				#print(t_grid.shape,self.times.shape)
-				grad_amp[i,:,j] = np.interp(t_grid, self.times * m_tot_us[i], grad_q_amp[i,:,j-1]* m_tot_us[i]/m_tot_std ,left = 0, right = 0) #set to zero outside the domain #(D,)
+				grad_amp[i,:,j] = np.interp(t_grid, self.times * m_tot_us[i], grad_q_amp[i,:,j-1],left = 0, right = 0) #set to zero outside the domain #(D,)
 				grad_ph[i,:,j]  = np.interp(t_grid, self.times * m_tot_us[i], grad_q_ph[i,:,j-1]) #(D,)
 
 		#dealing with gradients w.r.t. M
-		amp, ph = self.get_mode(theta, t_grid, out_type = "ampph", red_grid = False) #true wave evaluated at t_grid #(N,D)
+		amp, ph = self.get_mode(theta, t_grid, out_type = "ampph") #true wave evaluated at t_grid #(N,D)
 		for i in range(theta_std.shape[0]):
 			grad_M_amp = np.gradient(amp[i,:], t_grid) #(D,)
 			grad_M_ph = np.gradient(ph[i,:], t_grid) #(D,)
-			grad_amp[i,:,0] = amp[i,:]/m_tot_us[i] - np.multiply(t_grid/m_tot_us[i], grad_M_amp) #(D,)
-			grad_ph[i,:,0]  = -np.multiply(t_grid/m_tot_us[i], grad_M_ph) #(D,)
+			grad_amp[i,:,0] = - np.multiply(t_grid/m_tot_us[i]**2, grad_M_amp) #(D,)
+			grad_ph[i,:,0]  = -np.multiply(t_grid/m_tot_us[i]**2, grad_M_ph) #(D,)
 
 		grad_ph = np.subtract(grad_ph,grad_ph[:,0,None,:]) #unclear... but apparently compulsory
 			#check when grad is zero and keeping it
@@ -1014,17 +1012,24 @@ mode_generator
 		zero = np.where(diff== 0)
 		grad_ph[zero[0],zero[1],:] = 0 #takes care of the flat part after ringdown (gradient there shall be zero!!)
 
-		#computing gradients of the real and imaginary part
-		ph = np.subtract(ph.T,ph[:,0]).T
-		grad_Re = np.multiply(grad_amp, np.cos(ph)[:,:,None]) - np.multiply(np.multiply(grad_ph, np.sin(ph)[:,:,None]), amp[:,:,None]) #(N,D,4)
-		grad_Im = np.multiply(grad_amp, np.sin(ph)[:,:,None]) + np.multiply(np.multiply(grad_ph, np.cos(ph)[:,:,None]), amp[:,:,None])#(N,D,4)
 
+		if out_type == "ampph":
 			#switching back spins
 			#sure of it???
-		grad_Re[to_switch,:,2], grad_Re[to_switch,:,3] = grad_Re[to_switch,:,3], grad_Re[to_switch,:,2]
-		grad_Im[to_switch,:,2], grad_Im[to_switch,:,3] = grad_Im[to_switch,:,3], grad_Im[to_switch,:,2]
+			grad_amp[to_switch,:,2], grad_amp[to_switch,:,3] = grad_amp[to_switch,:,3], grad_amp[to_switch,:,2]
+			grad_ph[to_switch,:,2], grad_ph[to_switch,:,3] = grad_ph[to_switch,:,3], grad_ph[to_switch,:,2]
+			return grad_amp, grad_ph
+		if out_type == "realimag":
+			#computing gradients of the real and imaginary part
+			ph = np.subtract(ph.T,ph[:,0]).T
+			grad_Re = np.multiply(grad_amp, np.cos(ph)[:,:,None]) - np.multiply(np.multiply(grad_ph, np.sin(ph)[:,:,None]), amp[:,:,None]) #(N,D,4)
+			grad_Im = np.multiply(grad_amp, np.sin(ph)[:,:,None]) + np.multiply(np.multiply(grad_ph, np.cos(ph)[:,:,None]), amp[:,:,None])#(N,D,4)
 
-		return grad_Re, grad_Im
+			grad_Re[to_switch,:,2], grad_Re[to_switch,:,3] = grad_Re[to_switch,:,3], grad_Re[to_switch,:,2]
+			grad_Im[to_switch,:,2], grad_Im[to_switch,:,3] = grad_Im[to_switch,:,3], grad_Im[to_switch,:,2]
+			return grad_Re, grad_Im
+
+		return None, None
 
 
 

mlgw_v2/GW_helper.py
@@ -151,20 +151,20 @@ compute_optimal_mismatch
 
 ################# Dataset related stuff
 
-def create_dataset_TD_TEOBResumS(N_data, N_grid, mode = (2,2),filename = None,  t_coal = 0.5, q_range = (1.,5.), m2_range = None, s1_range = (-0.8,0.8), s2_range = (-0.8,0.8), t_step = 1e-5, alpha = 0.35, path_TEOBResumS = None):
+def create_dataset_TD(N_data, N_grid, mode = (2,2),filename = None,  t_coal = 0.5, q_range = (1.,5.), m2_range = None, s1_range = (-0.8,0.8), s2_range = (-0.8,0.8), t_step = 1e-5, alpha = 0.35, approximant = "TEOBResumS", path_TEOBResumS = None):
 	"""
-create_dataset_TD_TEOBResumS
-============================
-	Create a dataset for training a ML model to fit GW waveforms in time domain.
-	The dataset consists in 3 parameters theta=(q, spin1z, spin2z) associated to the waveform computed in frequency domain for a grid of N_grid points in the range given by the user.
+create_dataset_TD
+=================
+	Create a dataset for training a ML model to fit GW modes in time domain.
+	The dataset consists in 3 parameters theta=(q, spin1z, spin2z) associated to the modes computed in time domain for a grid of N_grid points in the range given by the user.
 	More specifically, data are stored in 3 vectors:
 		theta_vector	vector holding source parameters q, spin1, spin2
 		amp_vector		vector holding amplitudes for each source evaluated at some N_grid equally spaced points
 		ph_vector		vector holding phase for each source evaluated at some N_grid equally spaced points
 	This routine add N_data data to filename if one is specified (if file is not empty it must contain data with the same N_grid); otherwise the datasets are returned as np vectors. 
-	All the waves are evaluated at a constant distance of 1Mpc. Values of q and m2 as well as spins are drawn randomly in the range given by the user: it holds m1 = q *m2 M_sun.
-	The waveforms are computed with a time step t_step; starting from a frequency f_min (set by the routine according to t_coal and m_tot). Waves are given in a rescaled time grid (i.e. t/m_tot) with N_grid points: t=0 occurs when at time of maximum amplitude. A higher density of grid points is placed in the post merger phase.
-	Dataset is generated either with an implementation of TEOBResumS (a path to a local installation of TEOBResumS should be provided).
+	Values of q and m2 as well as spins are drawn randomly in the range given by the user: it holds m1 = q *m2 M_sun.
+	The waveforms are computed with a time step t_step; starting from a time in reduced grid tau min (s/M_Sun). Waves are given in a rescaled time grid (i.e. t/m_tot) with N_grid points: t=0 occurs when the 22 mode has a peak. A higher density of grid points is placed close to the merger.
+	Dataset is generated either with an implementation of TEOBResumS (a path to a local installation of TEOBResumS should be provided) either with a lal approximant (lalsuite installation should be provided). Unfortunately for lal, only the 22 mode is currently implemented.
 	Dataset can be loaded with load_dataset.
 	Input:
 		N_data				size of dataset
@@ -179,7 +179,8 @@ create_dataset_TD_TEOBResumS
 		t_step				time step to generate the wave with
 		approximant			string for the approximant model to be used (in lal convention; to be used only if lal ought to be used)
 		alpha				distorsion factor for time grid. (In range (0,1], when it's close to 0, more grid points are around merger)
-		path_TEOBResumS		path to a local installation of TEOBResumS with routine 'EOBRun_module' (if given, it overwrites the aprroximant entry)
+		approximant			lal approximant to be used for generating the modes, or "TEOBResumS" (in the latter case a local installation must be provided by the argument path_TEOBResumS) 
+		path_TEOBResumS		path to a local installation of TEOBResumS with routine 'EOBRun_module' (if given, it overwrites the aproximant entry)
 	Output:
 		if filename is given
 			None
@@ -224,11 +225,13 @@ create_dataset_TD_TEOBResumS
 
 	if not isinstance(mode,tuple):
 		raise TypeError("Wrong kind of mode {} given. Expected a tuple (l,m)".format(mode))
+
 	modes = [mode]
-	modes_to_k = lambda modes:[int(x[0]*(x[0]-1)/2 + x[1]-2) for x in modes] # [(l,m)] -> [k]
-	k_modes = modes_to_k(modes)
-	if modes != [(2,2)]:
-		k_modes.append(1) #22 modes is always computed
+	if approximant == "TEOBResumS": #computing k modes for TEOBResumS
+		modes_to_k = lambda modes:[int(x[0]*(x[0]-1)/2 + x[1]-2) for x in modes] # [(l,m)] -> [k]
+		k_modes = modes_to_k(modes)
+		if modes != [(2,2)]:
+			k_modes.append(1) #22 modes is always computed
 	print("Generating mode: "+str(modes[0]))
 
 		#creating time_grid
@@ -258,7 +261,7 @@ create_dataset_TD_TEOBResumS
 			print("New file ", filename, " created")
 			freq_header = np.concatenate((np.zeros((3,)), time_grid, time_grid) )
 			freq_header = np.reshape(freq_header, (1,len(freq_header)))
-			np.savetxt(filebuff, freq_header, header = "# row: theta "+str(D_theta)+" | amp (1,"+str(N_grid)+")| ph (1,"+str(N_grid)+")\n# N_grid = "+str(N_grid)+" | t_coal ="+str(t_coal)+" | t_step ="+str(t_step)+" | q_range = "+str(q_range)+" | m2_range = "+str(m2_range)+" | s1_range = "+str(s1_range)+" | s2_range = "+str(s2_range), newline = '\n')
+			np.savetxt(filebuff, freq_header, header = "# row: theta "+str(D_theta)+" | amp (None,"+str(N_grid)+")| ph (None,"+str(N_grid)+")\n# N_grid = "+str(N_grid)+" | t_coal ="+str(t_coal)+" | t_step ="+str(t_step)+" | q_range = "+str(q_range)+" | m2_range = "+str(m2_range)+" | s1_range = "+str(s1_range)+" | s2_range = "+str(s2_range), newline = '\n')
 		else:
 			filebuff = open(filename,'a')
 
@@ -295,12 +298,17 @@ create_dataset_TD_TEOBResumS
 			m1 = q * m2
 		else:
 			m1 = q* m2
+		nu = np.divide(q, np.square(1+q)) #symmetric mass ratio
 
 			#computing f_min
 		f_min = .9* ((151*(t_coal_freq)**(-3./8.) * (((1+q)**2)/q)**(3./8.))/(m1+m2))
 		 #in () there is the right scaling formula for frequency in order to get always the right reduced time
 		 #this should be multiplied by a prefactor (~1) for dealing with some small variation due to spins
-		#print(f_min, k_modes) #DEBUG
+
+		if isinstance(m2_range, tuple):
+			temp_theta = [m1, m2, spin1z, spin2z]		
+		else:
+			temp_theta = [m1/m2, spin1z, spin2z]
 
 			#getting the wave
 		if approximant == "TEOBResumS": #using TEOBResumS
@@ -322,16 +330,41 @@ create_dataset_TD_TEOBResumS
 				'inclination':inclination,
 			}
 			time_full, h_p, h_c, hlm = EOBRun_module.EOBRunPy(pars)
-			
-		if isinstance(m2_range, tuple):
-			temp_theta = [m1, m2, spin1z, spin2z]		
-		else:
-			temp_theta = [m1/m2, spin1z, spin2z]
+			temp_amp = hlm[str(k_modes[0])][0]*nu #TEOBResumS has weird conventions on the modes
+			temp_ph = hlm[str(k_modes[0])][1]
+			argpeak = locate_peak(hlm['1'][0]*nu) #aligned at the peak of the 22
 
-		temp_amp = hlm[str(k_modes[0])][0]
-		temp_ph = hlm[str(k_modes[0])][1]
 
-		argpeak = locate_peak(hlm['1'][0]) #aligned at the peak of the 22
+		if approximant != "TEOBResumS":
+			if modes != [(2,2)]:
+				raise ValueError("Currently lal approximants do not implement HMs")
+			hp, hc = lalsim.SimInspiralChooseTDWaveform( #where is its definition and documentation????
+				m1*lalsim.lal.MSUN_SI, #m1
+				m2*lalsim.lal.MSUN_SI, #m2
+				0., 0., spin1z, #spin vector 1
+				0., 0., spin2z, #spin vector 2
+				1e6*lalsim.lal.PC_SI, #distance to source
+				0., #inclination
+				0., #phi
+				0., #longAscNodes
+				0., #eccentricity
+				0., #meanPerAno
+				t_step, # time incremental step
+				f_min, # lowest value of freq
+				f_min, #some reference value of freq (??)
+				LALpars, #some lal dictionary
+				approx #approx method for the model
+			)
+			h_p, h_c =  hp.data.data, hc.data.data
+			time_full = np.linspace(0.0, hp.data.length*t_step, hp.data.length) #time grid at which wave is computed
+			temp_amp = np.sqrt(np.square(h_p)+np.square(h_c))
+			temp_ph = np.unwrap(np.arctan2(h_c,h_p))
+			argpeak = locate_peak(temp_amp) #aligned at the peak of the 22
+				#getting mode from WF
+			prefactor = 4.7864188273360336e-20 # G/c^2*(M_sun/Mpc)
+			amp_prefactor = prefactor*(m1+m2)/1. # G/c^2 (M / d_L)
+			temp_amp = temp_amp / amp_prefactor / (4*np.sqrt(5/(64*np.pi)))
+
 		t_peak = time_full[argpeak]
 		time_full = (time_full - t_peak)/(m1+m2) #grid is scaled to standard grid
 			#setting waves to the chosen std grid

mlgw_v2/fit_model.py
@@ -36,7 +36,6 @@ create_PCA_dataset
 		times					times at which waves are evalueted in the high dimensional representation (not useful for MoE but required by mlgw.GW_generator
 	Dataset will be saved to output folder in the following files (total 9):
 		"amp(ph)_PCA_model"    "PCA_train(test)_theta.dat"    "PCA_train(test)_amp(ph).dat"    "times"
-	While loading, amplitude are scaled by a factor of 1e-21 to make them O(1).
 	Input:
 		K (tuple)		number of PC to consider (K_amp, K_ph); if int amp and ph have the same number of PC
 		dataset_file	path to file holding input waveform dataset
@@ -52,6 +51,10 @@ create_PCA_dataset
 			return
 
 	theta_vector, amp_dataset, ph_dataset, times = load_dataset(dataset_file, shuffle=True) #loading dataset
+	if False: #weird thing to fix a different scaling in the previous dataset. User does not want to care about it
+		nu = np.divide(theta_vector[:,0], np.square(1+theta_vector[:,0]))
+		amp_dataset = np.multiply(amp_dataset.T, nu).T
+
 	print("Loaded datataset with shape: "+ str(ph_dataset.shape))
 
 	train_theta, test_theta, train_amp, test_amp = make_set_split(theta_vector, amp_dataset, train_frac, 1.)

mlgw_v2/test_HM.py
@@ -32,16 +32,16 @@ modes_to_k = lambda modes:[int(x[0]*(x[0]-1)/2 + x[1]-2) for x in modes] # [(l,m
 #	computing mismatch
 
 
-load = True		#whether to load the saved data
+load = False		#whether to load the saved data
 plot = False		#whether to plot the comparison between the WFs
 
 N_waves = 1500 #WFs to generate
-filename = "accuracy/mismatch_hist_TEOBResumS.dat"		#file to save the accuracy data to
-filename_theta = "accuracy/theta_hist_TEOBResumS.dat"	#file to save the orbital paramters the hist refers to
+filename = "accuracy/mismatch_hist_TEOBResumS_new.dat"		#file to save the accuracy data to
+filename_theta = "accuracy/theta_hist_TEOBResumS_new.dat"	#file to save the orbital paramters the hist refers to
 
 f_min = 10	#starting frequency for the WFs
 
-np.random.seed(4423) #optional, setting a random seed for reproducibility
+np.random.seed(2423) #optional, setting a random seed for reproducibility
 
 modes = [(2,2), (2,1), (3,3), (3,2), (3,1), (4,4), (4,3), (4,2), (4,1), (5,5)]	#modes to inspect
 
@@ -71,7 +71,9 @@ if not load:
 		h_TEOB = h_p_TEOB + 1j*h_c_TEOB
 
 			#computing overall mismatch
-		print("it: {} \n  Theta: {}\n   q: {}".format(i, theta[i,:],np.maximum(theta[i,0]/theta[i,1],theta[i,1]/theta[i,0])))
+		q = np.maximum(theta[i,0]/theta[i,1],theta[i,1]/theta[i,0])
+		nu = np.divide(q, np.square(1+q))
+		print("it: {} \n  Theta: {}\n   q: {}".format(i, theta[i,:],q))
 
 		ids = np.where(times < 1e-4)[0] #cutting TEOB WFs beacause they have problems...
 		res = scipy.optimize.minimize_scalar(compute_mismatch, bounds = [0.,2*np.pi],
@@ -106,7 +108,7 @@ if not load:
 
 			#removing the shit of TEOBResumS
 			where_zero = np.where(amp_mlgw[:,j] == 0)[0]
-			amp_TEOB =  hlm[str(k[0])][0]
+			amp_TEOB =  hlm[str(k[0])][0]*nu #TEOBResumS convention is weird...
 			ph_TEOB = hlm[str(k[0])][1]
 			amp_TEOB[where_zero] = 0
 			ph_TEOB[where_zero] = ph_TEOB[where_zero[0]]
@@ -172,11 +174,13 @@ print("Loading histogram from: {}\n\t{} datapoints".format(filename, F.shape[0])
 
 	#overall mismatch
 plt.figure(figsize = (6.4, 4.8/2.))
+plt.subplots_adjust(hspace = .8)
 plt.title("Overall mismatch", fontsize = 15)
 plt.hist(np.log10(F[:,0]), bins = 70, color = 'k', density = True)
 plt.xlabel(r"$\log \; \bar{\mathcal{F}}$")
-plt.savefig("accuracy/mismatch_overall.pdf", format = 'pdf')
 plt.xlim([-6,0])
+plt.tight_layout()
+plt.savefig("accuracy/mismatch_overall.pdf", format = 'pdf', transparent=True)
 
 	#optimal mismatch
 fig, ax_list = plt.subplots(figsize = (6.4,1.5*6.4), nrows = len(modes), ncols = 1, sharex = True)
@@ -189,7 +193,30 @@ for i in range(len(modes)):
 	ax_list[i].set_title("Mode {}".format(modes[i]), fontsize = 12)
 
 
-plt.savefig("accuracy/mismatch_HMs.pdf", format = 'pdf')
+plt.savefig("accuracy/mismatch_HMs.pdf", format = 'pdf', transparent=True)
+
+
+	#optimal mismatch
+fig, ax_list = plt.subplots(figsize = (6.4,1.*6.4), nrows = int(len(modes)/2+.5), ncols = 2, sharex = True)
+plt.subplots_adjust(hspace = 0.6)
+plt.suptitle("HMs mismatch", fontsize = 15)
+ax_list[-1,0].set_xlabel(r"$\log \; \bar{\mathcal{F}}$")
+ax_list[-1,1].set_xlabel(r"$\log \; \bar{\mathcal{F}}$")
+
+for i in range(ax_list.shape[0]):
+	for j in range(ax_list.shape[1]):
+		k = ax_list.shape[1]*i + j
+		if k >= len(modes):
+			ax_list[i,j].cla()
+			ax_list[i,j].axis('off')
+			continue
+		ax_list[i,j].hist(np.log10(F[:,k+1]), bins = 70, color = 'k', density = True)
+		ax_list[i,j].set_title("Mode {}".format(modes[k]), fontsize = 12)
+
+
+plt.savefig("accuracy/mismatch_HMs_twocols.pdf", format = 'pdf', transparent=True)
+
+
 
 	#contour plots
 fig, ax_list = plt.subplots(figsize = (1.5*6.4,1.5*6.4), nrows = 4, ncols = 3)
@@ -213,7 +240,7 @@ for i in range(ax_list.shape[0]):
 		ax_list[i,j].set_xlabel(r"$q$")
 		ax_list[i,j].set_ylabel(r"$s_1$")
 
-plt.savefig("accuracy/countor_HMs.pdf", format = 'pdf')
+plt.savefig("accuracy/countor_HMs.pdf", format = 'pdf', transparent=True)
 
 plt.show()
 