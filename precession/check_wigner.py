c954ed95068f9aed91fccc201183a44d9b670d1c
stefanoschmidt1995 <stefanoschmidt1995@gmail.com>
2021-04-15 18:46:57 +0200

    Added Wigner checks

--- precession/check_wigner.py
+++ precession/check_wigner.py
@@ -1,23 +1,35 @@
-#TODO: try to understand how the Wigner matrices work. It is not very clear how the inverse of Wigner's matrix is defined
+"""
+This script checks the robustness of the my implementation of Wigner functions.
+The benchmark is from the package spherical_functions: https://github.com/moble/spherical_functions
+Apparently everything works quite well (apart from a complex conjugate)
+"""
 
+import spherical_functions as sf #goes only with numpy == 1.20.2
 import numpy as np
-import matplotlib.pyplot as plt
-
 import sys
 sys.path.insert(0,'../mlgw_v2')
 
 import GW_generator as gen
+g = gen.GW_generator(1)
 
-g = gen.GW_generator()
+for i in range(10000):
 
-m_list = [-2,-1,1,2]
-beta = np.array([0.])
+	alpha, beta, gamma =  np.array([np.random.uniform(0,2*np.pi)]),  np.array([np.random.uniform(0,2*np.pi)]), np.array([np.random.uniform(0,2*np.pi)])
+	ell = np.random.randint(5) + 2
+	mp,m = [np.random.randint(ell+1)],[np.random.randint(ell+1)]
+	mp[0] *= (np.random.randint(2)*2-1)
+	m[0] *= (np.random.randint(2)*2-1)
+	
+	wig_package = sf.Wigner_D_element(alpha[0], beta[0], gamma[0], ell, mp[0], m[0])
 
-d_mprime_m = np.zeros((len(m_list),len(m_list)))
+	wig_mine = g._GW_generator__get_Wigner_D_matrix(ell,mp,m, alpha,beta,gamma)
 
-for i, m_prime_ in enumerate(m_list):
-	for j, m_ in enumerate(m_list):
-		d_mprime_m[i,j] = g._GW_generator__get_Wigner_d_function(2, m_list[i],m_list[j], beta)
+	all_close = np.allclose(wig_package, np.conj(wig_mine), rtol = 1e-5)
+	print("Iteration: ",i)
+	print("l,m,mp ",ell,mp,m)
+	print("\t",all_close, wig_mine, wig_package)
 
-print(d_mprime_m)
-print(np.einsum('ij,ik->jk',d_mprime_m,d_mprime_m))
+	assert all_close
+
+#print("package wigner ", wig_package)
+#print("my wigner ", wig_mine)
