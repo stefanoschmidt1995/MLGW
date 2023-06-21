9a3899a5d3f33c829ec1b02487a7004db08a23dc
TimGrimbergen <86293103+TimGrimbergen@users.noreply.github.com>
2023-06-21 13:48:02 +0200

    Update create_res_models.py

--- mlgw_NN/code/create_res_models.py
+++ mlgw_NN/code/create_res_models.py
@@ -11,7 +11,7 @@ param_dict = {'layer_list' : [[15,15,15,15]],
               'optimizers' : [("Nadam",0.0006)],
               'activation' : ['sigmoid'],
               'batch_size' : [128],
-              'schedulers' : [PCAdata_v2.Schedulers('exponential',exp=-0)],
+              'schedulers' : [Schedulers('exponential',exp=-0)],
               'loss_functions' : [('custom_mse',[10,2],0,2)]}
 
 NeuralNetwork.HyperParametertesting(data_loc, save_loc, "ph", param_dict, 2, epcs=6000, feat=["chirp","2nd_poly","eff_spin","sym_mas","eff_spin_sym_mas_2nd_poly","eff_spin_sym_mas_3rd_poly","eff_spin_sym_mas_4th_poly"])
